26de6e142956d8a5871119c19a516434e14d07ec
django_freeman <o.tampodo.t.18@gmail.com>
2023-10-07 18:43:15 +0900

    debug

--- pages/api/incr.ts
+++ pages/api/incr.ts
@@ -7,9 +7,9 @@ export const config = {
 };
 
 export default async function incr(req: NextRequest): Promise<NextResponse> {
-	if (req.method !== "POST") {
-		return new NextResponse("use POST", { status: 405 });
-	}
+	// if (req.method !== "POST") {
+	// 	return new NextResponse("use POST", { status: 405 });
+	// }
 	if (req.headers.get("Content-Type") !== "application/json") {
 		return new NextResponse("must be json", { status: 400 });
 	}
